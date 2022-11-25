d26a201f5d93137f6962c9588947331b827a5480
TomJohn <115263325+TomJohnH@users.noreply.github.com>
2022-11-25 01:54:22 +0100

    Add files via upload
    Issues with footer removal

--- streamlit_app.py
+++ streamlit_app.py
@@ -206,9 +206,8 @@ if start:
 
 hide_streamlit_style = """
             <style>
-            .reportview-container .main footer {visibility: hidden;} 
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
-# MainMenu {visibility: hidden;}
