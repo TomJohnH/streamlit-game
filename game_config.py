43d5131256d8506b09c77519fcd939df9dbde0b9
TomJohnH <thasiow@onet.pl>
2023-01-17 22:57:23 +0100

    updated intro graphic

--- game_config.py
+++ game_config.py
@@ -2,7 +2,7 @@ caption_below_input = 'Use mouse or [Tab] to focus on input field. To check pote
 # future improvements: all texts will be transferred to database this will clean up the code
 
 image_source = {
-    "introScene": "images/path.jpg",
+    "introScene": "images/crossroad_mid.png",
     "sheepScene": "images/sheep.jpg",
     "caveScene": "images/cave.jpg",
     "poScene": "images/po.jpg",
