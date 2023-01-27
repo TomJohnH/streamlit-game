18cd0e7f7b11e487787acded727132bbf1357333
TomJohnH <thasiow@onet.pl>
2023-01-27 20:23:32 +0100

    badge patch

--- streamlit_app.py
+++ streamlit_app.py
@@ -156,7 +156,7 @@ st.markdown(
     f"""
     <div class="bpad" id="bpad">
     <a href="https://www.buymeacoffee.com/tomjohn" style="color: grey; text-decoration:none;">
-    <div style="justify-content: center;margin:10px; border:solid 2px; ;border-radius:10px; border-color:#21212f; width: fit-content;padding:10px">
+    <div style="justify-content: center;margin:0px; border:solid 2px; ;border-radius:10px 10px 0 0; border-color:#21212f; width: fit-content;padding:0.425rem">
     <img src="https://raw.githubusercontent.com/TomJohnH/streamlit-game/main/images/coffe.png" style="max-width:20px;margin-right:10px;">
     Buy me a coffee</a></div></div>""",
     unsafe_allow_html=True,
